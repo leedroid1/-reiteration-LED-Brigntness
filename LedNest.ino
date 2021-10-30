8d9da6d9c0d491fea67f224f929e7333ddbd90ae
leedroid1 <89372422+leedroid1@users.noreply.github.com>
2021-10-31 03:21:10 +0900

    Update LedNest.ino

--- LedNest.ino
+++ LedNest.ino
@@ -1,21 +1,29 @@
-int led = 5;
-int sensor = 3;
-int Brightness = 0;
+#include <Time.h>
+float onTime = 0;
+float outTime = 0;
+float Time = 0;
+boolean temp = 0;
+const int sensor = 3;
+const int led = 5;
 
 void setup() {
-pinMode(sensor, INPUT);
-pinMode(led, OUTPUT);
-Serial.begin(9600);
+  pinMode(sensor, INPUT);
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-if (digitalRead(sensor) == 0) {
-  Brightness += 1;
-  analogWrite(led, Brightness);
-  Serial.println(Brightness);
-  delay(500);
-}
-else if(Brightness > 20) {
-  Brightness = 0;
-}
+  boolean check = digitalRead(sensor);
+
+  if(check == 1 && temp == 0) {
+    onTime = millis();
+    temp = 1;
+  }
+  else if(check == 0 && temp == 1) {
+    outTime = millis();
+    Time = ((onTime - outTime) / 100) + 10;
+    Serial.println(Time);
+    temp = 0;
+  }
+      delay(10);
 }
